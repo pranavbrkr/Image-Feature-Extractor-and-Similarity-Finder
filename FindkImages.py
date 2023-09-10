6ad6aec48e9f53f7f0a1e451fb9dee686c722907
pranavbrkr <pranavbrkr@gmail.com>
2023-09-10 01:31:08 -0700

    store non-RGB images

--- FindkImages.py
+++ FindkImages.py
@@ -2,6 +2,7 @@ from ExtractFeatureDescriptors import loadDataset
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 client = MongoClient()
 client = MongoClient(host="localhost", port=27017)
@@ -12,25 +13,36 @@ image_id = int(input("Enter Image ID of the image: "))
 
 k = int(input("Enter value of k: "))
 
-db = client.MWD_Phase_1
+db = client.Multimedia_Web_DBs
 
-feature_descriptors = list(db.feature_descriptors.find({}))
+feature_descriptors = list(db.Feature_Descriptors.find({}))
 n = len(feature_descriptors)
 
 euclidean_distances = []
+cosine_distances = []
 
 for i in range(n):
   if i == image_id:
     continue
 
-  euclidean_distances.append({"id": feature_descriptors[i]["_id"],  "similarity": math.dist(feature_descriptors[image_id]["layer3"], feature_descriptors[i]["layer3"])})
+  euclidean_distances.append({"id": feature_descriptors[i]["_id"],  "similarity": math.dist(feature_descriptors[image_id]["hog"], feature_descriptors[i]["hog"])})
+  cosine_distances.append({"id": feature_descriptors[i]["_id"],  "similarity": (np.dot(feature_descriptors[image_id]["hog"], feature_descriptors[i]["hog"]) / (np.linalg.norm(feature_descriptors[image_id]["hog"]) * np.linalg.norm(feature_descriptors[i]["hog"])))})
+
+print(euclidean_distances)
+print(cosine_distances)
 
 euclidean_similarity = sorted(euclidean_distances, key=lambda x: x["similarity"], reverse=False)
+cosine_similarity = sorted(cosine_distances, key=lambda x: x["similarity"], reverse=False)
 
-plt.imshow(caltectDataset[image_id].permute(1, 2, 0))
+print(euclidean_similarity)
+print(cosine_similarity)
+plt.imshow(caltectDataset[image_id][1].permute(1, 2, 0))
 plt.show()
 
-for i in range(k):
-  print(euclidean_similarity[i])
-  plt.imshow(caltectDataset[euclidean_similarity[i]["id"]].permute(1, 2, 0))
+for index in range(k):
+  print(euclidean_similarity[index])
+  plt.imshow(caltectDataset[euclidean_similarity[index]["id"]][1].permute(1, 2, 0))
+  plt.show()
+  print(cosine_similarity[index])
+  plt.imshow(caltectDataset[cosine_similarity[index]["id"]][1].permute(1, 2, 0))
   plt.show()
--- StoreToMongo.py
+++ StoreToMongo.py
@@ -15,8 +15,8 @@ feature_descriptors = db.Feature_Descriptors
 
 n = len(caltectDataset)
 
-for image_id in range(1470, n):
-  if checkChannel(caltectDataset[image_id][1]):
+for image_id in range(n):
+  if not checkChannel(caltectDataset[image_id][1]):
     data = {
       "_id": image_id,
       "color_moments": extractCM10x10(caltectDataset[image_id][1]),
@@ -25,12 +25,15 @@ for image_id in range(1470, n):
       "layer3": extractResnetLayer3(caltectDataset[image_id][1]),
       "fc": extractResnetFc(caltectDataset[image_id][1]),
     }
+  else:
+    data = {
+      "_id": image_id,
+      "hog": extractHOG(caltectDataset[image_id][1])
+    }
 
-    if feature_descriptors.find_one({"_id": image_id}):
-      feature_descriptors.update_one({"_id": image_id}, {"$set": data})
-      print(f"Updated feature descriptors for Image {image_id}")
-    else:
-      feature_descriptors.insert_one(data)
-      print(f"Inserted feature descriptors for Image {image_id}")
- 
-print(f"{(time.time() - start_time)} seconds")
+  if feature_descriptors.find_one({"_id": image_id}):
+    feature_descriptors.update_one({"_id": image_id}, {"$set": data})
+    print(f"Updated feature descriptors for Image {image_id}")
+  else:
+    feature_descriptors.insert_one(data)
+    print(f"Inserted feature descriptors for Image {image_id}")
