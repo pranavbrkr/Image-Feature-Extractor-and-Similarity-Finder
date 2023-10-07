8f8e702d4c3a83d492e7797d588bf68623ad6d96
pranavbrkr <pranavbrkr@gmail.com>
2023-10-07 13:55:47 -0700

    task6 and task3

--- phase2/task6.py
+++ phase2/task6.py
@@ -1,15 +1,14 @@
 import json
+import math
 from pymongo import MongoClient
-from task0a import *
 import scipy
 import numpy as np
 from sklearn.decomposition import NMF
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.cluster import KMeans
 
-
 client = MongoClient()
-client = MongoClient(host="localhost", port=27017)
+client = MongoClient(host = "localhost", port = 27017)
 
 # Select the database
 db = client.Multimedia_Web_DBs
@@ -18,43 +17,64 @@ db = client.Multimedia_Web_DBs
 feature_descriptors = list(db.Caltech101_Feature_Descriptors.find({}))
 feature_descriptors = sorted(list(db.Caltech101_Feature_Descriptors.find({})), key=lambda x: x["_id"], reverse=False)
 
-num_labels = 101
+def extractKLatentSemantics(k, image_sim_matrix, feature_model, dim_reduction):
 
-def extractKLatentSemantics(k, feature_model, dim_reduction):
-
-  feature_vectors = [x[feature_model] for x in feature_descriptors if x["_id"] % 2 == 0]
-  feature_labels = [x["label"] for x in feature_descriptors if x["_id"] % 2 == 0]
   feature_ids = [x["_id"] for x in feature_descriptors if x["_id"] % 2 == 0]
+  feature_labels = [x["label"] for x in feature_descriptors if x["_id"] % 2 == 0]
 
-  filename = 'ls1.json'
-
+  filename = 'ls4.json'
 
   match dim_reduction:
 
     case 1:
-      U, S, Vh = scipy.sparse.linalg.svds(np.array(feature_vectors), k=k)
+      U, S, Vh = scipy.sparse.linalg.svds(np.array(image_sim_matrix), k=k)
       k_latent_semantics = sorted(list(zip(feature_ids, U.tolist())), key = lambda x: x[1][0], reverse = True)
 
     case 2:
       model = NMF(n_components = k, init = 'random', solver = 'cd', alpha_H = 0.01, alpha_W = 0.01, max_iter = 10000)
-      min_value = np.min(feature_vectors)
-      feature_vectors_shifted = feature_vectors - min_value
+      min_value = np.min(image_sim_matrix)
+      feature_vectors_shifted = image_sim_matrix - min_value
       U = model.fit_transform(np.array(feature_vectors_shifted))
       k_latent_semantics = sorted(list(zip(feature_ids, U.tolist())), key = lambda x: x[1][0], reverse = True)
 
     case 3:
-      U = LinearDiscriminantAnalysis(n_components = k).fit_transform(feature_vectors, feature_labels)
+      U = LinearDiscriminantAnalysis(n_components = k).fit_transform(image_sim_matrix, feature_labels)
       k_latent_semantics = sorted(list(zip(feature_ids, U.tolist())), key = lambda x: x[1][0], reverse = True)
 
     case 4:
       kmeans = KMeans(n_clusters = k)
-      kmeans.fit(feature_vectors)
-      U = kmeans.transform(feature_vectors)
+      kmeans.fit(image_sim_matrix)
+      U = kmeans.transform(image_sim_matrix)
       k_latent_semantics = sorted(list(zip(feature_ids, U.tolist())), key = lambda x: x[1][0], reverse = True)
   
   k_latent_semantics = [{"_id": item[0], "semantics": item[1]} for item in k_latent_semantics]
   with open(filename, 'w', encoding='utf-8') as f:
     json.dump(k_latent_semantics, f, ensure_ascii = False)
+
+def findImageImageSimMatrix(feature_model):
+   
+  feature_vectors = [x[feature_model] for x in feature_descriptors if x["_id"] % 2 == 0]
+
+  n = len(feature_vectors)
+
+  image_sim_matrix = np.zeros((n, n))
+
+  for i in range(n):
+    for j in range(i + 1, n):
+
+      match feature_model:
+
+        case "color_moments":
+          image_sim_matrix[i][j] = image_sim_matrix[j][i] = math.dist(feature_vectors[i], feature_vectors[j])
+        
+        case "hog":
+          image_sim_matrix[i][j] = image_sim_matrix[j][i] = (np.dot(feature_vectors[i], feature_vectors[j]) / (np.linalg.norm(feature_vectors[i]) * np.linalg.norm(feature_vectors[j])))
+
+        case "avgpool" | "layer3" | "fc":
+          image_sim_matrix[i][j] = image_sim_matrix[j][i] = scipy.stats.pearsonr(feature_vectors[i], feature_vectors[j]).statistic
+          
+  return image_sim_matrix
+
 
 def main():
 
@@ -76,7 +96,10 @@ def main():
   print("4. k-means")
   dim_reduction = int(input("Select the dimensionality reduction technique: "))
 
-  extractKLatentSemantics(k, feature_model, dim_reduction)
+  image_sim_matrix = findImageImageSimMatrix(feature_model)
+  print(image_sim_matrix)
+
+  extractKLatentSemantics(k, image_sim_matrix, feature_model, dim_reduction)
 
   
 
